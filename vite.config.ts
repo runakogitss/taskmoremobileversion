a47b5d46cf34a3d668a21c813b000533cc35f05d
Reynar D. Runako <159867487+runakogitss@users.noreply.github.com>
2025-07-05 09:50:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/taskmoremobile-ver',
+  base: process.env.VITE_BASE_PATH || '/taskmoremobileversion',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
